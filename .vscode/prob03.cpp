1d1f5b11699b62be2ebc5ceedae3ef9d213a0619
goohanseo <103124688+goohanseo@users.noreply.github.com>
2022-09-05 12:45:55 +0900

    과제 1,2 테스트 데이터 통과

--- .vscode/prob03.cpp
+++ .vscode/prob03.cpp
@@ -2,19 +2,17 @@
 #include <stdio.h>
 int main(void){
     int list[100];
+    printf("정수를 입력하시오:");
     for(int i =0; i<100; i++){
         scanf("%d",&list[i]);
-        if(
         for(int j =0; j<i; j++){
-        list[j] == list[i];})
-        printf("duplicate");
+        if (list[j] == list[i])
+        {
+        printf("duplicate");}
         else
-        
         for (int n = 0; n<=i; n++ ){
-        printf("%d ",list[n]);}
+        printf("%d ",list[n]);}}
     }
-<<<<<<< Updated upstream
-}ㅁㄴㅇㄹ
-=======
+    return 0;
 }
->>>>>>> Stashed changes
+
