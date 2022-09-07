974b1bf78fcb32761a5f2a2a69e25f38619af0f3
goohanseo <103124688+goohanseo@users.noreply.github.com>
2022-09-07 20:34:14 +0900

    문제 5까지 완료

--- .vscode/prob/prob05.cpp
+++ .vscode/prob/prob05.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector <int> v;
 int main(void){
     int n;
-    print("입력될 정수의 개수(n<100)를 입력하시오");
+    printf("입력할 갯수 n을 입력하시오:\n");
     scanf("%d",&n);
-    int list[n];
-    for(i =0; i<n, i++){
-        scanf("%d", %list[i]);
-    }
-
+    
+    for(int i=0; i<n; i++)
+        for(i=0; i<n; i++){
+            int tmp;
+		scanf("%d", &tmp);
+		v.push_back(tmp);}
+    sort(v.begin(),v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
     printf("%d: ",n);
-    for(i =0; i<n, i++){
-        printf("%d", %list[i]);
-    }
+    for (int i=0;i<v.size();i++)
+    printf("%d ",v[i]);
+    printf("\n");
+    return 0;
 }
