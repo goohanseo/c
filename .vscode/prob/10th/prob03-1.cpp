8044e69437c7ab8ec9db8ed979117bdb8f9fee0f
goohanseo <103124688+goohanseo@users.noreply.github.com>
2022-11-12 18:10:07 +0900

    3333

--- .vscode/prob/10th/prob03-1.cpp
+++ .vscode/prob/10th/prob03-1.cpp
@@ -11,19 +11,21 @@ FILE * file;
 
 typedef struct data_case{ //data_case 구조체
     int index;            //배열 갯수
-    long argument[BUFFER_SIZE]; //배열 저장
+    int *argument; //배열 저장
 } *Data_case;
 
 Data_case case_array[BUFFER_SIZE]; //Data_case 배열
+
 int n; //전체 테스트케이스 갯수
 int num =0; //테스트케이스 배열
 int m = 0; //h함수 구하기 위한 정수
-int tmp_index = 0; //각 테스트케이스당 인자 갯수
 
 int main(){
     open_file();
     for(int i = 0; i < n; i++){
+        printf("nowfinished\n");
         calculate();
+        m++;
     }
     return 0;
 }
@@ -37,43 +39,47 @@ void open_file(){
     else{
         printf("open file\n");
         organize_file();
-        fclose(file);
+        
     }
 }
 
 void organize_file(){
-    printf("organize success\n");
+    //printf("organize success\n");
     fscanf(file,"%d ", &n);
     for(int i=0; i<n; i++){
-        printf("%d,  %d\n",n,i);
-        tmp_index = 0;
-        fscanf(file,"%d ", &tmp_index);
-        create_case();
-        for(int j=0; j<tmp_index; j++){
-            int tmp_argument = 0;
-            fscanf(file,"%d ", &tmp_argument);
-            case_array[num]->argument[j] = tmp_argument;
-        }
+        //printf("%d,  %d\n",n,i);
         num++;
+        case_array[num] = (Data_case)malloc(sizeof(Data_case));
+        case_array[num]->argument = (int *)malloc(BUFFER_SIZE * sizeof(int));
+        fscanf(file,"%d ", &case_array[num]->index);
+        //printf("%d\n ", case_array[num]->index);
+        //create_case();
+        for(int j=0; j<case_array[num]->index; j++){
+            fscanf(file,"%d ", &case_array[num]->argument[j]);
+        }
+        printf("%d\n ", case_array[num]->index);
+        printf("%d-finished\n",i);
     }
+    
+    fclose(file);
 }
 
-void create_case(){
-    case_array[num] = (Data_case)malloc(sizeof(Data_case));
-    case_array[num]->index = tmp_index;
-    printf("%d  ", case_array[num]->index);
-    printf("create success\n");
-}
+// void create_case(){
+//     // ++num;
+//     // case_array[num] = (Data_case)malloc(sizeof(Data_case));
+//     case_array[num]->index = case_array[num]->index;
+//     printf("%d-", case_array[num]->index);
+//     printf("create\n");
+//     }
 
 void calculate(){
     int tmp = 0; 
     for(int j = (case_array[m]->index)-1; j >= 0; j--){ //구조체 index값만큼 뒤에서부터 검사
         int k = 1; 
-        while(case_array[m]->argument[j]>=case_array[m]->argument[j-k] && j-k >= 0){ //현재 배열보다 앞 배열이 작으면 tmp++
+        while((case_array[m]->argument[j])>=(case_array[m]->argument[j-k]) && j-k >= 0){ //현재 배열보다 앞 배열이 작으면 tmp++
             tmp++; //argument[index]~[0] 다 더하고 
             k++;
         }
     } 
     printf("%d\n", tmp%1000000); //출력하기
-    m++;
 }
--- .vscode/prob/10th/tempCodeRunnerFile.cpp
+++ .vscode/prob/10th/tempCodeRunnerFile.cpp
@@ -1,1 +1,2 @@
- // organize_file();
+++num;
+    // case_array[num] = (Data_case)malloc(sizeof(Data_case));
